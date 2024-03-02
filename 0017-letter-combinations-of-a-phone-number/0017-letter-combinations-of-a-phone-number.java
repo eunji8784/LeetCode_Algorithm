47ea98d83b9400bf31b85de73814582de4039b46
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2024-03-03 08:52:01 +0900

    Time: 4 ms (45.38%), Space: 42.2 MB (45.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,31 @@
 import java.util.*;
 
 class Solution {
-    private static final char[][] DIGITS = { {}, {}, { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' },
-            { 'j', 'k', 'l' }, { 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' }, { 't', 'u', 'v' }, { 'w', 'x', 'y', 'z' } };
+    private static final String[] DIGITS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-    private static int n;
     private static List<String> result;
-    private static int[] digit;
 
     public List<String> letterCombinations(String digits) {
-        n = digits.length();
-        result = new ArrayList<>();
-
-        if (n != 0) {
-            digit = new int[n];
-            for (int i = 0; i < n; i++) {
-                digit[i] = digits.charAt(i) - '0';
-            }
-            solve(0, "");
+        if (digits.isEmpty()) {
+            return Collections.emptyList();
         }
-
+        
+        result = new ArrayList<>();
+        solve(0, "", digits);
+        
         return result;
     }
 
-    private static void solve(int index, String letter) {
-        if (index == n) {
+    private static void solve(int index, String letter, String digits) {
+        if (index == digits.length()) {
             result.add(letter);
             return;
         }
 
-        char[] arr = DIGITS[digit[index]];
+        String arr = DIGITS[digits.charAt(index) - '0'];
 
-        for (int i = 0; i < arr.length; i++) {
-            solve(index + 1, letter + arr[i]);
+        for (int i = 0; i < arr.length(); i++) {
+            solve(index + 1, letter + arr.charAt(i), digits);
         }
     }
 }
