e2c9843e29ecae61edc1ee54938ec2149fddd614
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2024-04-25 17:45:45 +0900

    Time: 16 ms (64.70%), Space: 43.6 MB (97.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,30 +2,17 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         final int INF = Integer.MAX_VALUE;
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, INF);
-        
-        if (amount == 0) {
-            return 0;
-        }
-        
-        for (int coin : coins) {
-            if (coin <= amount) {
-                dp[coin] = 1; 
-            }
-        }
+        Arrays.fill(dp, INF - 1);
+        dp[0] = 0;
         
         for (int i = 1; i <= amount; i++) {
-            if (dp[i] == 1) {
-                continue;
-            }
-            int n = i / 2;
-            for (int j = 1; j <= n; j++) {
-                if (dp[i - j] != INF && dp[j] != INF) {
-                    dp[i] = Math.min(dp[i], dp[i - j] + dp[j]);
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
         
-        return (dp[amount] == INF ? -1 : dp[amount]);
+        return (dp[amount] == INF - 1 ? -1 : dp[amount]);
     }
 }
